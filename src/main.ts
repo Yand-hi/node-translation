741a88fc28da5b637bd3919b4c3f6bf93a87d6d7
Yandhi <15877410869@163.com>
2022-01-17 22:18:13 +0800

    Support Chinese <=> English translation

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,36 @@ import {appId, appSecret} from './private';
 
 export const translate = (word) => {
   const errorMap = {
-    52001:'请求超时，请重试 ',
-    52002:'系统错误，请重试 ',
-    52003:'未授权用户，请检查appid是否正确或者服务是否开通 ',
-    54000:'必填参数为空，请检查是否少传参数',
-    54001:'签名错误，请检查您的签名生成方法',
+    52001: '请求超时，请重试',
+    52002: '系统错误，请重试',
+    52003: '未授权用户，请检查appid是否正确或者服务是否开通',
+    54000: '必填参数为空，请检查是否少传参数',
+    54001: '签名错误，请检查您的签名生成方法',
     54003: '访问频率受限，请降低您的调用频率，或进行身份认证后切换为高级版/尊享版',
     54004: '账户余额不足，请前往管理控制台为账户充值',
     54005: '长query请求频繁， 请降低长query的发送频率，3s后再试',
-    58000:'客户端IP非法，检查个人资料里填写的IP地址是否正确，可前往开发者信息-基本信息修改',
-    58001:' 译文语言方向不支持，检查译文语言是否在语言列表里',
-    58002:'服务当前已关闭，请前往管理控制台开启服务 ',
-    90107:'认证未通过或未生效，请前往我的认证查看认证进度 ',
+    58000: '客户端IP非法，检查个人资料里填写的IP地址是否正确，可前往开发者信息-基本信息修改',
+    58001: '译文语言方向不支持，检查译文语言是否在语言列表里',
+    58002: '服务当前已关闭，请前往管理控制台开启服务',
+    90107: '认证未通过或未生效，请前往我的认证查看认证进度',
   };
   const salt = Math.random();
   const sign = md5(appId + word + salt + appSecret);
+  let from, to;
+  if (/[a-zA-Z]/.test(word[0])) {
+    from = 'en';
+    to = 'zh';
+  } else {
+    from = 'zh';
+    to = 'en';
+  }
   const query: string = queryString.stringify({
     q: word,
-    from: 'en',
-    to: 'zh',
     appid: appId,
-    salt: salt,
-    sign: sign
+    from,
+    to,
+    salt,
+    sign
   });
   const options = {
     hostname: 'api.fanyi.baidu.com',
@@ -57,7 +65,9 @@ export const translate = (word) => {
         console.error(errorMap[object.error_code] || object.error_msg);
         process.exit(2);
       } else {
-        console.log(object.trans_result[0].dst);
+        object.trans_result.map(obj => {
+          console.log(obj.dst);
+        });
         process.exit(0);
       }
     });
