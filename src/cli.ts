1a746ae3f0dcd37359b13de5da2c316fe163cd02
Yandhi <15877410869@163.com>
2022-01-17 20:49:09 +0800

    init

---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+import {Command} from 'commander';
+import {translate} from './main';
+
+const program = new Command();
+
+program.version('0.0.1')
+  .name('fy')
+  .usage('<English>')
+  .argument('<English>')
+  .action((english) => {
+    translate(english);
+  });
+
+program.parse(process.argv);
